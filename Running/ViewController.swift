420911b4a3415ff81d0f7b19f31d2a37e8aeaf56
romainhild <romain.hild@me.com>
2018-04-27 16:15:56 +0200

    Initial Commit

---
+++ Running/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running
+//
+//  Created by Romain Hild on 27/04/2018.
+//  Copyright © 2018 Romain Hild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
